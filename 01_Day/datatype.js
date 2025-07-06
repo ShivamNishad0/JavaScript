3bdcc835e07ebd39e65884ccf2a63bc2fa22f71d
Ishu <ishugupta526@gmail.com>
2025-07-06 15:16:17 +0530

    second commit

---
+++ 01_Day/datatype.js
@@ -0,0 +1,23 @@
+"use strict"; // This line enables strict mode, which helps catch common coding errors and "unsafe" actions such as defining global variables unintentionally.
+
+//alert(3+3) This will show an alert with the result of 3 + 3, which is 6 but in a browser environment, not in Node.js.
+console.log(3+3) // This will log the result of 3 + 3
+
+
+let name = "Shivam Nishad";
+let age =23;
+let isLoggedIn = false;
+
+/*
+number => 2 the power of 53; 
+BigInt => 2 the power of 53 + 1;
+string => "Shivam Nishad";
+boolean => true or false;
+null => empty value(stadlone);
+undefined => variable is declared but not initialized;
+Symbol => unique identifier;
+Object => collection of key-value pairs;
+*/
+
+console.log(name, age, isLoggedIn);
+console.log(typeof name); // string
---
+++ 01_Day/variable.js
@@ -0,0 +1,38 @@
+const accountId = 8887756715;
+let accountEmail ="sn.shivamni@gmail.com";
+var accountPassword = "Shrihari@123";
+accountCity = "Kanpur";
+let accountState;
+
+//accountId = 6387023796; // This will throw an error because accountId is a constant(we can not change the value of the constant variale)
+console.log(accountId);
+
+
+accountEmail ="2k23.mca23136722@gmail.com"; // This is allowed because accountEmail is a let variable and we can change its value
+console.log(accountEmail);
+
+accountPassword = "Shivam@1234"; // This is allowed because accountPassword is a var variable and we can change its value
+console.log(accountPassword);
+
+accountCity = "Delhi"; // This is allowed because accountCity is a global variable and we can change its value
+console.log(accountCity);
+
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+/*
+
+Note: In JavaScript, we can declare variables using var, let, and const.
+- var: Function scoped or globally scoped, can be re-declared and updated.
+- let: Block scoped, can be updated but not re-declared in the same scope.
+- const: Block scoped, cannot be updated or re-declared, must be initialized at the time of declaration.
+- Global variables can be declared without var, let, or const, but it's not recommended as it can lead to unexpected behavior and conflicts in larger codebases.
+
+Best practices:
+1. Use const for variables that should not change.
+2. Use let for variables that will change.
+3. Avoid using var to prevent issues with variable hoisting and scope.
+
+*/
+
