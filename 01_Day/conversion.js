a029a25484da5e582ad2faad67371b2815cc8604
ShivamNishad0 <2k23.mca23136722@gmail.com>
2025-07-06 17:03:28 +0530

    new user

---
+++ 01_Day/conversion.js
@@ -0,0 +1,4 @@
+console.log("Shivam Nishad");
+console.log("Welcome to the JavaScript course!");
+console.log("This is a test file to check the console output.");
+console.log("You can run this file using Node.js or in a browser console.");
